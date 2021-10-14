f2f43ccd1e2652ded961a7477050bcda3f37ae2e
Khaing-myae <khaingmyae.sket@gmail.com>
2021-10-14 14:31:08 +0900

    Contact form

--- contact.js
+++ contact.js
@@ -5,34 +5,61 @@ function ShowHideDiv() {
 }
 var phoneNumber = document.getElementById("phone_number")
 var checkedPhone = document.getElementById("checked-phone");
+var email = document.getElementById("email")
+var validEmail = document.getElementById("valid_email");
+var confirmEmail = document.getElementById("confirm_email");
+var checkedEmail = document.getElementById("checked-email");
+
+    //To get Phone Number Validation
       phoneNumber.onkeyup = function() {
-      var numbers = /^[0-9]+$/;
+      var numbers = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
             if(phoneNumber.value.match(numbers)) { 
-              return true;
+              document.getElementById("checked-phone").style.display = "none";
             } else{ 
-              checkedPhone.style.display = "block";
+              document.getElementById("checked-phone").style.display = "block";
               return false;
             } 
           }  
 
- var email = document.getElementById("email")
- var validEmail = document.getElementById("valid_email");
+    //To get Email Validation
   email.onkeyup = function() {
   var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(email.value.match(emailFormat)) { 
-        return true;
+        document.getElementById("valid_email").style.display = "none";
     } else{ 
-      validEmail.style.display = "block";
+      document.getElementById("valid_email").style.display = "block";
       return false;
      } 
-}  
+} 
+  confirmEmail.onkeyup = function(){
+    if(email.value !== confirmEmail.value) { 
+      checkedEmail.style.display ="block";
+  } else if(email.value == confirmEmail.value){ 
+      checkedEmail.style.display ="none";
+   }  
+  }
+  
 var contactForm = document.getElementById("contact_form")
 contactForm.addEventListener('submit',onsubmit);
 function onsubmit(e){
   e.preventDefault();
   var firstName = document.getElementById("firstName").value;
   var lastName = document.getElementById("lastName").value;
+  var gender1 = document.getElementById("gender-1");
+  var gender2 = document.getElementById("gender-2");
+  var zip1 = document.getElementById("zip1").value;
+  var zip2 = document.getElementById("zip2").value;
+  var addressText = document.getElementById("address-text").value;
+  var phoneNumber = document.getElementById("phone_number").value;  
+  var email = document.getElementById("email").value;
+  var confirmEmail = document.getElementById("confirm_email").value;
+  var checkedEmail = document.getElementById("checked-email");
+  var level_boxText = document.getElementById("level_box").value;
+  var output_levelWarp = document.getElementById("output-level_warp");
+  var contact_contentText = document.getElementById("contact_content").value;
+  var checkbox = document.getElementById("checkbox");
 
+    //To get Name
       if(firstName, lastName === '') {
        window.alert('お名前を入れてください。');
         return false;
@@ -40,20 +67,17 @@ function onsubmit(e){
         document.getElementById("output-name").innerHTML = firstName + " " + lastName;
       }
 
-      var gender1 = document.getElementById("gender-1");
-      var gender2 = document.getElementById("gender-2");
+    //To get Gender 
       if(gender1.checked == true) {
         document.getElementById("output-gender").innerHTML = "男性";
       } else {
         document.getElementById("output-gender").innerHTML = "女性";
       }
   
-  var zip1 = document.getElementById("zip1").value;
-  var zip2 = document.getElementById("zip2").value;
-  var addressText = document.getElementById("address-text").value;
+    //To get Address
     document.getElementById("output-address").innerHTML = "テ"+ zip1 +" －"+ + zip2 + addressText;
 
-  var phoneNumber = document.getElementById("phone_number").value;    
+    //To get Phone Number
       if(phoneNumber === '') {
         window.alert('電話番号を入れてください。');
         return false;
@@ -61,11 +85,7 @@ function onsubmit(e){
         document.getElementById("output-phone").innerHTML = phoneNumber;
       } 
       
-
-  var email = document.getElementById("email").value;
-  var confirmEmail = document.getElementById("confirm_email").value;
-  var checkedEmail = document.getElementById("checked-email");
-  
+    //To get Email
       if(email !== confirmEmail) {
         checkedEmail.style.display = "block";
         return false;
@@ -78,6 +98,7 @@ function onsubmit(e){
         document.getElementById("output-confirmEmail").innerHTML = confirmEmail;
       }
        
+    //To get Contact Type
       if (document.getElementById("contact_type").value == "勉強会について") {
        document.getElementById("output-contact-type").innerHTML = "勉強会について";
       }
@@ -103,32 +124,35 @@ function onsubmit(e){
        document.getElementById("output-contact-type").innerHTML = "それぞれのご意見";
       } 
 
-  var level_boxText = document.getElementById("level_box").value;
-  if(level_boxText === '') {
+  //To get Level Box
+  if(level_boxText === '' && (document.getElementById("contact_type").value == "新しいクラスの情報")) {
     window.alert('レベルと資格を入れてください。');
     return false;
-  } else {
+  } if (document.getElementById("contact_type").value != "新しいクラスの情報"){
+    document.getElementById("output-level_warp").style.display = "none";
+  } else  {
+    document.getElementById("output-level_warp").style.display = "block";
     document.getElementById("output-level_box").innerHTML = level_boxText;
-  }
+  } 
 
-  var contact_contentText = document.getElementById("contact_content").value;
+  //To get Contact Content
   document.getElementById("output-contact-content").innerHTML = contact_contentText;
 
-  var checkbox = document.getElementById("checkbox");
+  //To get Checkbox
   if(checkbox.checked == true) {
     document.getElementById("output-checkbox").innerHTML = "true";
   } else {
     document.getElementById("output-checkbox").innerHTML = "false";
   }
 
-      var x, y;
-      x = document.getElementById("contact-data-input");
-      y = document.getElementById("contact-data-output");
+      var contact_dataInput, contact_dataOutput;
+      contact_dataInput = document.getElementById("contact-data-input");
+      contact_dataOutput = document.getElementById("contact-data-output");
 
 
-      if (y.style.display === "none") {
-        y.style.display = "block";
-        x.style.display = "none"
+      if (contact_dataOutput.style.display === "none") {
+        contact_dataOutput.style.display = "block";
+        contact_dataInput.style.display = "none"
       }
 }
 
